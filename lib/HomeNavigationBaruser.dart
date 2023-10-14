828974891bfc2b891d25888e4fb85b6ed7afc398
diptomistry <diptomistrybd@gmail.com>
2023-10-15 04:36:18 +0600

    dd

--- lib/HomeNavigationBaruser.dart
+++ lib/HomeNavigationBaruser.dart
@@ -1,7 +1,54 @@
+// import 'package:coltech/add_post/NewsFeed/HomePage.dart';
+// import 'package:coltech/expert/expertHomePage.dart';
+// import 'package:coltech/profile/profile_screen.dart';
+// import 'package:coltech/userprofile.dart';
+// import 'package:coltech/userprofile2.dart';
+// import 'package:coltech/users/userHomePage.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class UserNav extends StatefulWidget {
+//   const UserNav({super.key});
+//
+//   @override
+//   State<UserNav> createState() => _UserNavState();
+// }
+//
+// class _UserNavState extends State<UserNav> {
+//   int _selectedIndex = 0;
+//   final List<Widget> _pages = [
+//     UserHomePage(),
+//     //HomePage(),
+//     UserProfile(),
+//   ];
+//
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _pages[_selectedIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: _selectedIndex,
+//         items: [
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+//           //BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+//           BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+//         ],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
 import 'package:coltech/add_post/NewsFeed/HomePage.dart';
 import 'package:coltech/expert/expertHomePage.dart';
 import 'package:coltech/profile/profile_screen.dart';
 import 'package:coltech/userprofile.dart';
+import 'package:coltech/userprofile2.dart';
 import 'package:coltech/users/userHomePage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -18,13 +65,18 @@ class _UserNavState extends State<UserNav> {
   final List<Widget> _pages = [
     UserHomePage(),
     //HomePage(),
-    UserProfile(),
+    ProfileScreen(), // Changed to ProfileScreen
   ];
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    if (index == 1) {
+      // If the person icon is pressed, navigate back.
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   }
 
   @override
@@ -35,7 +87,7 @@ class _UserNavState extends State<UserNav> {
         currentIndex: _selectedIndex,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
+          //BottomNavigationBarItem(icon: Icon(Icons.search), label: ''),
           BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
         ],
         onTap: _onItemTapped,
--- lib/expertNavBar.dart
+++ lib/expertNavBar.dart
@@ -1,3 +1,49 @@
+// import 'package:coltech/add_post/NewsFeed/HomePage.dart';
+// import 'package:coltech/expert/expertHomePage.dart';
+// import 'package:coltech/profile/profile_screen.dart';
+// import 'package:coltech/userprofile.dart';
+// import 'package:coltech/users/userHomePage.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'expertprofile.dart';
+// import 'expertprofile2.dart';
+//
+// class ExpertNav extends StatefulWidget {
+//   const ExpertNav({super.key});
+//
+//   @override
+//   State<ExpertNav> createState() => _ExpertNavState();
+// }
+//
+// class _ExpertNavState extends State<ExpertNav> {
+//   int _selectedIndex = 0;
+//   final List<Widget> _pages = [
+//     expertHomePage(),
+//     ExpartProfile2 (email: '',),
+//   ];
+//
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: _pages[_selectedIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         currentIndex: _selectedIndex,
+//         items: [
+//           BottomNavigationBarItem(icon: Icon(Icons.home), label: ''),
+//           BottomNavigationBarItem(icon: Icon(Icons.person), label: ''),
+//         ],
+//         onTap: _onItemTapped,
+//       ),
+//     );
+//   }
+// }
 import 'package:coltech/add_post/NewsFeed/HomePage.dart';
 import 'package:coltech/expert/expertHomePage.dart';
 import 'package:coltech/profile/profile_screen.dart';
@@ -20,13 +66,18 @@ class _ExpertNavState extends State<ExpertNav> {
   int _selectedIndex = 0;
   final List<Widget> _pages = [
     expertHomePage(),
-    ExpartProfile2(email: '',),
+    ProfileScreen(), // Changed to ProfileScreen
   ];
 
   void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
+    if (index == 1) {
+      // If the person icon is pressed, navigate back.
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   }
 
   @override
